1a36344e30e4d9d87321107463bf38ec031a0a4c
DavidNovo <david.novogrodsky@gmail.com>
2015-07-20 21:15:37 -0500

    more clean up of the objects and the index page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -21,12 +21,12 @@ workRecent.position = "software Engineer";
 workRecent.city = "San Antonio";
 
 // bracket notation
-var education = {};
-education["schoolName"] = "Udacity";
-education["yearsAttended"] = "2014 - present";
-education["schoolCity"] = "Mountain View";
+var educationBracket = {};
+educationBracket["schoolName"] = "Udacity";
+educationBracket["yearsAttended"] = "2014 - present";
+educationBracket["schoolCity"] = "Mountain View";
 
-var education2 = {
+var education = {
     "schools": [
         {
             "schoolName": "Udacity",
@@ -45,25 +45,28 @@ var education2 = {
     ],
     "onlineCourses": [
         {
-            "title": "Javascript Crash Course"
+            "title": "Javascript Crash Course",
+            "school" : "Udacity",
+            "dates" : "current",
+            "URL" : ""
         }
     ]
 };
 
 var work ={
-    "start" : "",
-    "employer" : "",
-    "title": "",
-    "dates" : "",
-    "location" : "",
-    "description" : ""
+    "start" : "2015 June",
+    "employer" : "Peer 1 Hosting",
+    "title": "Software Engineer",
+    "dates" : "current",
+    "location" : "San Antonio",
+    "description" : "It is a lot of fun"
 };
 
 var project = {
-    "start" : "",
-    "title" : "",
-    "dates" : "",
-    "description" : "",
+    "start" : "2015, June",
+    "title" : "Make a temperature sensor using Explora",
+    "dates" : "on-going",
+    "description" : "I want to make a weather station using an Explora board.  I am using one of them because I do not know hoiw to wire the electronics parts I will need.",
     "image" : ""
 };
 
@@ -77,8 +80,8 @@ var HTMLworkEmployer    =  HTMLworkEmployer.replace('%data%' ,workRecent.company
 var HTMLworkTitle   = HTMLworkTitle.replace('%data%', workRecent.position);
 var HTMLworkLocation    = HTMLworkLocation.replace('%data%', workRecent.city);
 
-var HTMLschoolName = HTMLschoolName.replace('%data%', education["schoolName"]);
-var HTMLschoolLocation = HTMLschoolLocation.replace('%data%', education["schoolCity"]);
+var HTMLschoolName = HTMLschoolName.replace('%data%', educationBracket["schoolName"]);
+var HTMLschoolLocation = HTMLschoolLocation.replace('%data%', educationBracket["schoolCity"]);
 
 
 $("#header").append(HTMLheaderName);
