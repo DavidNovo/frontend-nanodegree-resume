95d400451e769af4c4d594f51157de761330bbfe
DavidNovo <david.novogrodsky@gmail.com>
2015-07-20 21:19:30 -0500

    i more clean up of the file  Please enter the commit message for your changes. Lines starting

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -54,42 +54,26 @@ var education = {
 };
 
 var work ={
+    "jobs" : [
     "start" : "2015 June",
     "employer" : "Peer 1 Hosting",
     "title": "Software Engineer",
     "dates" : "current",
     "location" : "San Antonio",
     "description" : "It is a lot of fun"
+    ]
 };
 
-var project = {
+var projects = {
+    "projects" : [
     "start" : "2015, June",
     "title" : "Make a temperature sensor using Explora",
     "dates" : "on-going",
     "description" : "I want to make a weather station using an Explora board.  I am using one of them because I do not know hoiw to wire the electronics parts I will need.",
     "image" : ""
+    ]
 };
 
 
 
 
-var HTMLheaderName    = HTMLheaderName.replace('%data%' , bio.name);
-var HTMLheaderRole      = HTMLheaderRole.replace('%data%' , bio.role);
-
-var HTMLworkEmployer    =  HTMLworkEmployer.replace('%data%' ,workRecent.company);
-var HTMLworkTitle   = HTMLworkTitle.replace('%data%', workRecent.position);
-var HTMLworkLocation    = HTMLworkLocation.replace('%data%', workRecent.city);
-
-var HTMLschoolName = HTMLschoolName.replace('%data%', educationBracket["schoolName"]);
-var HTMLschoolLocation = HTMLschoolLocation.replace('%data%', educationBracket["schoolCity"]);
-
-
-$("#header").append(HTMLheaderName);
-$("#header").append(HTMLheaderRole);
-
-$("#header").append(HTMLworkEmployer);
-$("#header").append(HTMLworkTitle);
-$("#header").append(HTMLworkLocation);
-
-$("#header").append(HTMLschoolName);
-$("#header").append(HTMLschoolLocation);
